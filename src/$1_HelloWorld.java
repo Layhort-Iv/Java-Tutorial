7ca25ead9e84c41c0867c59115fec1de8d812814
Iv Layhort <ivlayhort18@kit.edu.kh>
2020-05-11 23:09:44 +0700

    Java Tutorial

---
+++ src/$1_HelloWorld.java
@@ -0,0 +1,21 @@
+public class $1_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.print("Welcome to Java Programming Language");
+    }
+}
+
+/*
+ Public is an access modifier, class is a java keyword for creating class
+ HelloWorld is a class name.
+ void is a return type, main is a function name
+ String[] args is an array of string
+
+ System is an object, out is the properties of system object
+ println() is a method to print something to the terminal with the new line.
+
+ Things to take away:
+ - Every valid Java Application must have a class definition (that matches the filename).
+ - The main method must be inside the class definition.
+ - The compiler executes the codes starting from the main function.
+ */
